c525814b517c1d3d64a8093b9b322679080204b7
CENGİZHAN <CENGZHAN@DESKTOP-5G1UH9B>
2025-09-22 10:37:08 +0300

    Initialize layered architecture structure for C# .NET Core project

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+//ProductTest();
+CategoryTest();
+static void ProductTest()
+{
+    ProductManager productManager = new ProductManager(new EfProductDal());
+
+    foreach (var product in productManager.GetByUnitPrice(30, 120))
+    {
+        Console.WriteLine(product.ProductName);
+    }
+}
+
+static void CategoryTest()
+{
+    CategoryManager x = new CategoryManager(new EfCategoryDal());
+    foreach (var y in x.GetAll())
+    {
+        Console.WriteLine(y.CategoryName);
+    }
+}
---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,78 @@
+﻿using Core.DataAcccess;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework
+{ 
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class ,IEntity , new()
+        where TContext : DbContext, new()
+
+    {
+        public void Add(TEntity X)
+        {
+            using (TContext Y = new TContext())
+            {
+                var addedEntity = Y.Entry(X);
+                addedEntity.State = EntityState.Added;
+                Y.SaveChanges();
+            }
+        }
+
+       
+
+        public void Delete(TEntity X)
+        {
+            using (TContext Y = new TContext())
+            {
+                var deletedEntity = Y.Entry(X);
+                deletedEntity.State = EntityState.Deleted;
+                Y.SaveChanges();
+            }
+        }
+
+      
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext Y = new TContext())
+            {
+                return Y.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+      
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext Y = new TContext())
+            {
+                return filter == null
+                    ? Y.Set<TEntity>().ToList()
+                    : Y.Set<TEntity>().Where(filter).ToList();
+            }
+
+        }
+
+        
+
+        public void Update(TEntity X)
+        {
+            using (TContext Y = new TContext())
+            {
+                var uptadedEntity = Y.Entry(X);
+                uptadedEntity.State = EntityState.Modified;
+                Y.SaveChanges();
+            }
+        }
+
+
+}
+
+}
